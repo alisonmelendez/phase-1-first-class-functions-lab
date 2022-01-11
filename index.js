fce53e9b47b01c800833f30c161d999013f779ac
Alison Melendez <alisonmelendez132@gmail.com>
2022-01-11 17:06:32 -0500

    commit tree 749f2c6293e931454629bd3638c9a92a600ffcd9

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function fareMultiplier(fare) {
+        //let fare
+
+
+
+        return fare * multiplier //does the fare raised to the power of the multiplier
+
+    }
+}
+
+const toBeReturned = function() {
+
+}
